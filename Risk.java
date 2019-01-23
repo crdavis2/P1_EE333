355e7e7db6ee40797a4a434146961890505ab8bd
crdavis2 <42817669+crdavis2@users.noreply.github.com>
2019-01-22 22:43:36 -0600

    Create Risk.java

---
+++ Risk.java
@@ -0,0 +1,14 @@
+/*
+ * File: Risk.java
+ * Author: Collin Davis crdavis2@uab.edu
+ * Assignment: P1
+ * Vers: 1.0.0 01/22/2019 crd - initial coding
+ */
+
+/**
+ * Generic Risk
+ * @author Collin Davis
+ */
+public class Risk {
+    
+}

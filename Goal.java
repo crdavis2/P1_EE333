ff3368198e6f3bb1d564c86e354197ec152212a0
crdavis2 <42817669+crdavis2@users.noreply.github.com>
2019-01-22 22:43:24 -0600

    Create Goal.java

---
+++ Goal.java
@@ -0,0 +1,14 @@
+/*
+ * File: Goal.java
+ * Author: Collin Davis crdavis2@uab.edu
+ * Assignment: P1
+ * Vers: 1.0.0 01/22/2019 crd - initial coding
+ */
+
+/**
+ * Generic Goal
+ * @author Collin Davis
+ */
+public class Goal {
+    
+}
